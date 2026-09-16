bcf96c22b236a919127c2b40eac28cc6dd6cb6ad
jgabryelp <joaogabryelcaldeirap@gmail.com>
2026-09-16 17:39:59 -0300

    primeira consulta

--- main.py
+++ main.py
@@ -4,24 +4,49 @@ import pandas as pd
 
 
 #conectando com o banco de dados do MySQL
-#conexao = pyodbc.connect(       
- #   "DRIVER={MySQL ODBC 26.7 Unicode Driver};"  
-  #  "SERVER=localhost;"
-   # "DATABASE=spotifydatabase;"
-    #"UID=root;"
-    #"PWD=26022010jj;"
-#)
+conexao = pyodbc.connect(       
+    "DRIVER={MySQL ODBC 26.7 Unicode Driver};"  
+    "SERVER=localhost;"
+    "DATABASE=spotifydatabase;"
+    "UID=root;"
+    "PWD=26022010jj;"
+)
 
-#cursor = conexao.cursor() #criando o cursor
+cursor = conexao.cursor() #abrindo o cursor
 
-#cursor.execute("SELECT * FROM spotifytracks LIMIT 100")
-#dados = cursor.fetchall()
-#print(dados)
+#ranking de artistas com no mínimo 5 músicas por maiores médias de popularidade
+cursor.execute(""" 
+SELECT CASE WHEN INSTR(tks.artists, ';') > 0 THEN LEFT(tks.artists, INSTR(tks.artists, ";") - 1)   -- selecionando apenas o artista principal da faixa
+			ELSE tks.artists
+END AS "artists",  AVG(tdt.popularity) AS "popularity"       -- selecionando a média de popularidade
+FROM tracks AS tks INNER JOIN tracksdetails AS tdt        -- juntando as informações das duas tabelas
+ON tks.track_id = tdt.track_id                     -- as colunas que se correspondem nas tabelas (primary key e foreign key)
+GROUP BY CASE 
+        WHEN INSTR(tks.artists, ';') > 0 THEN LEFT(tks.artists, INSTR(tks.artists, ';') - 1)   -- selecionando novamente o primeiro artista pois o groupby é "executado" antes do select pelo sql 
+        ELSE tks.artists
+    END 
+HAVING COUNT(tks.track_name) >= 5 ORDER BY popularity DESC LIMIT 20;   -- limitando pra artistas com 5 musicas mínimas e ordenando por popularidade
+""")
 
-#cursor.close() #fechando o cursor
-#conexao.close() #fechando a conexão
+dados = cursor.fetchall() #armazenando os dados da consulta em uma variavel
+tabeladados = pd.DataFrame(dados) #criando uma tabela do pandas com os dados
+print(tabeladados)
 
-#artistas mais populares com x musicas minimas
+
+
+#artistas mais populares de cada genero
+cursor.execute("""
+                SELECT * FROM tracks LIMIT 10
+
+""")
+
+dados = cursor.fetchall()
+tabeladados = pd.DataFrame(dados)
+print(tabeladados)
+
+
+#artistas mais populares com x musicas minimas  CHECKS
+
 #artistas mais populares de cada genero
 #artistas mais versateis 
 #generos mais populares
@@ -30,3 +55,5 @@ import pandas as pd
 #caracteristicas das musicas mais DANÇANTES
 #musicas mais populares de cada genero
 
+cursor.close() #fechando o cursor
+conexao.close() #fechando a conexão
