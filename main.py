ed5f86fbfbbdbaddad1f93f983c50f0d09255f45
jgabryelp <joaogabryelcaldeirap@gmail.com>
2026-09-18 15:59:05 -0300

    segunda consulta

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ conexao = pyodbc.connect(
 cursor = conexao.cursor() #abrindo o cursor
 
 #ranking de artistas com no mínimo 5 músicas por maiores médias de popularidade
-cursor.execute(""" 
+artistasmaispopulares = """ 
 SELECT CASE WHEN INSTR(tks.artists, ';') > 0 THEN LEFT(tks.artists, INSTR(tks.artists, ";") - 1)   -- selecionando apenas o artista principal da faixa
 			ELSE tks.artists
 END AS "artists",  AVG(tdt.popularity) AS "popularity"       -- selecionando a média de popularidade
@@ -26,30 +26,32 @@ GROUP BY CASE
         ELSE tks.artists
     END 
 HAVING COUNT(tks.track_name) >= 5 ORDER BY popularity DESC LIMIT 20;   -- limitando pra artistas com 5 musicas mínimas e ordenando por popularidade
-""")
+"""
 
-dados = cursor.fetchall() #armazenando os dados da consulta em uma variavel
-tabeladados = pd.DataFrame(dados) #criando uma tabela do pandas com os dados
-print(tabeladados)
+#dados = cursor.fetchall() #armazenando os dados da consulta em uma variavel
+tabelaartistasmaispopulares = pd.read_sql_query(artistasmaispopulares, conexao) #criando uma tabela do pandas com os dados
+print(tabelaartistasmaispopulares)
 
 
 
-#artistas mais populares de cada genero
-cursor.execute("""
-                SELECT * FROM tracks LIMIT 10
+#generos mais populares
+generosmaispopulares = """
+                SELECT tks.track_genre AS "genre", COUNT(*) AS "tracks_quantity", 
+CONCAT(ROUND((SUM(tdt.popularity >= 80) / COUNT(*) * 100), 2), "%") AS "popular_percent"  -- selecionando as colunas de genero e criando as colunas de percentual de musicas populares e quantidade de musicas 
+FROM tracks AS tks INNER JOIN tracksdetails AS tdt ON tks.track_id = tdt.track_id   -- juntando as duas tabelas
+GROUP BY genre ORDER BY popular_percent DESC LIMIT 20;  -- cada genero aparecendo uma única vez
 
-""")
+"""
 
-dados = cursor.fetchall()
-tabeladados = pd.DataFrame(dados)
-print(tabeladados)
+tabelagenerosmaispopulares = pd.read_sql_query(generosmaispopulares, conexao)
+print(tabelagenerosmaispopulares)
 
 
 #artistas mais populares com x musicas minimas  CHECKS
 
+#generos mais populares
 #artistas mais populares de cada genero
 #artistas mais versateis 
-#generos mais populares
 #identidade sonora de cada genero
 #caracteristas das musicas mais populares vs das musicas menos populares
 #caracteristicas das musicas mais DANÇANTES
